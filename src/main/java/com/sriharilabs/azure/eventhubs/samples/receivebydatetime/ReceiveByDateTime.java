c7eddf1916a63b9fbb7733f34be796f6ce56ad93
srihari <gsriharijava@gmail.com>
2018-07-24 14:08:38 +0530

    azure-eventhub-receiveusingoffset
    azure-eventhub-receiveusingoffset

---
+++ src/main/java/com/sriharilabs/azure/eventhubs/samples/receivebydatetime/ReceiveByDateTime.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project root for full license information.
+ */
+package com.sriharilabs.azure.eventhubs.samples.receivebydatetime;
+
+import com.microsoft.azure.eventhubs.ConnectionStringBuilder;
+import com.microsoft.azure.eventhubs.EventData;
+import com.microsoft.azure.eventhubs.EventHubClient;
+import com.microsoft.azure.eventhubs.EventHubException;
+import com.microsoft.azure.eventhubs.EventPosition;
+import com.microsoft.azure.eventhubs.EventHubRuntimeInformation;
+import com.microsoft.azure.eventhubs.PartitionReceiver;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.time.Instant;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ReceiveByDateTime {
+
+    public static void main(String[] args)
+            throws EventHubException, ExecutionException, InterruptedException, IOException {
+
+//    	String consumerGroupName = "$Default";
+//    	String namespaceName = "nameshpace1";
+//    	String eventHubName = "eventhub1";
+//    	String sasKeyName ="RootManageSharedAccessKey";         
+//    	String sasKey = "f5n3qZD1V2Obfcguh3vjpAzYhnPZaqxMf7sDDGn67Gc=";   
+//    	String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=gstore1;AccountKey=KuhV+KYq9QbC0CJ9lr5b4cccuyHK37OFIl27ApP3UisxPx1ncyEdDf2BjluL+brfBcWFpDeI1dATTG4Ck/KbTQ==;EndpointSuffix=core.windows.net";//"----StorageContainerName----";
+//    	String storageContainerName="nameshpace1";
+//    	String hostNamePrefix ="sriharilabs-host";
+    	
+    	 final ConnectionStringBuilder connStr = new ConnectionStringBuilder()
+                 .setNamespaceName("nameshpace1") // to target National clouds - use .setEndpoint(URI)
+                 .setEventHubName("eventhub1")
+                 .setSasKeyName("RootManageSharedAccessKey")
+                 .setSasKey("f5n3qZD1V2Obfcguh3vjpAzYhnPZaqxMf7sDDGn67Gc=");
+
+        final ExecutorService executorService = Executors.newSingleThreadExecutor();
+        final EventHubClient ehClient = EventHubClient.createSync(connStr.toString(), executorService);
+
+        final EventHubRuntimeInformation eventHubInfo = ehClient.getRuntimeInformation().get();
+        final String partitionId = eventHubInfo.getPartitionIds()[0]; // get first partition's id
+
+        final PartitionReceiver receiver = ehClient.createEpochReceiverSync(
+                EventHubClient.DEFAULT_CONSUMER_GROUP_NAME,
+                partitionId,
+                EventPosition.fromEnqueuedTime(Instant.EPOCH),
+                2345);
+
+        System.out.println("date-time receiver created...");
+
+        try {
+            int receivedCount = 0;
+            while (receivedCount++ < 100) {
+                receiver.receive(100)
+                        .thenAcceptAsync(receivedEvents -> {
+                            int batchSize = 0;
+                            if (receivedEvents != null) {
+                                for (EventData receivedEvent : receivedEvents) {
+                                    System.out.print(String.format("Offset: %s, SeqNo: %s, EnqueueTime: %s",
+                                            receivedEvent.getSystemProperties().getOffset(),
+                                            receivedEvent.getSystemProperties().getSequenceNumber(),
+                                            receivedEvent.getSystemProperties().getEnqueuedTime()));
+
+                                    if (receivedEvent.getBytes() != null)
+                                        System.out.println(String.format("| Message Payload: %s", new String(receivedEvent.getBytes(), Charset.defaultCharset())));
+                                    batchSize++;
+                                    
+                                }
+                            }
+
+                            System.out.println(String.format("ReceivedBatch Size: %s", batchSize));
+                        }, executorService).get();
+            }
+        } finally {
+            // cleaning up receivers is paramount;
+            // Quota limitation on maximum number of concurrent receivers per consumergroup per partition is 5
+            receiver.close()
+                    .thenComposeAsync(aVoid -> ehClient.close(), executorService)
+                    .whenCompleteAsync((t, u) -> {
+                        if (u != null) {
+                            // wire-up this error to diagnostics infrastructure
+                            System.out.println(String.format("closing failed with error: %s", u.toString()));
+                        }
+                    }, executorService).get();
+
+            executorService.shutdown();
+        }
+    }
+
+}
